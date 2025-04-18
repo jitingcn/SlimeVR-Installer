293bda3701adcf716da31a59cead59d4755bec95
Butterscotch! <bscotchvanilla@gmail.com>
2025-04-18 13:08:45 -0400

    Select server v0.14.1

--- windows/web/download_offline_files.ps1
+++ windows/web/download_offline_files.ps1
@@ -59,7 +59,7 @@ Function Get-File-From-Uri {
 $DestDir = New-Item (& { $Env:WINDOWS_OFFLINE_FILES_DIR ?? "offline-files" }) -ItemType directory -Force
 Write-Output "Output directory set to ""$DestDir"""
 
-$ServerVersion = "v0.14.0"
+$ServerVersion = "v0.14.1"
 $JavaVersion = "17.0.15_6"
 $DriverVersion = "v0.2.2"
 $FeederVersion = "v0.2.11"
