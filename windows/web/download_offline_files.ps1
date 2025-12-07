11c2e00fe680ea7d345894a3506c32825fbedcb4
寂听 <jiting@jtcat.com>
2025-12-08 00:03:08 +0900

    bump server to v18.0.0-rc.1

--- windows/web/download_offline_files.ps1
+++ windows/web/download_offline_files.ps1
@@ -59,7 +59,7 @@ Function Get-File-From-Uri {
 $DestDir = New-Item (& { $Env:WINDOWS_OFFLINE_FILES_DIR ?? "offline-files" }) -ItemType directory -Force
 Write-Output "Output directory set to ""$DestDir"""
 
-$ServerVersion = "v0.17.0"
+$ServerVersion = "v18.0.0-rc.1"
 $JavaVersion = "17.0.16+8"
 $DriverVersion = "v0.3.1"
 $FeederVersion = "v0.2.15"
